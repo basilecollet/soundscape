54dc94f11f8fc8e6639375986676b6fdd6e553b0
Basile COLLET <60740802+basilecollet@users.noreply.github.com>
2025-06-27 10:30:09 +0200

    Update app.php
    change condition for prod env

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        if ($this->app->environment('production')) {
+        if (env('APP_ENV') === 'production') {
             $middleware->trustProxies(at: env('CC_REVERSE_PROXY_IPS'));
         }
     })
