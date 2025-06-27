7ac12092c8263a2c76e743fc6f5c4b6092e78f04
Basile COLLET <60740802+basilecollet@users.noreply.github.com>
2025-06-27 10:26:00 +0200

    Update app.php
    Add trusted proxies for clever cloud

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        if ($this->app->environment('production')) {
+            $middleware->trustProxies(at: env('CC_REVERSE_PROXY_IPS'));
+        }
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
